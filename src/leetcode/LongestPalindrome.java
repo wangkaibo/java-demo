5a3396a3f56f4275858b51d727ee20781c03fbfb
wangkaibo <wkaiboo@gmail.com>
2022-02-09 23:03:37 +0800

    longest palindrome string

---
+++ src/leetcode/LongestPalindrome.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            int len1 = expendAroundCenter(s, i, i);
+            int len2 = expendAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expendAroundCenter(String s, int left, int right) {
+        // a | a  a b a
+        int l = left, r = right;
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+            System.out.println(l);
+            System.out.println(r);
+        }
+        return r - l - 1;
+    }
+}
